871f9c36c0eb3473f8a3467fb9ecd973ff8d1bb0
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-05 16:29:53 +0100

    Add background & ground with automatic scaling and confirgurable horizotnal scolling

---
+++ Assets/Scripts/BackgroundRepeater.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundRepeater : MonoBehaviour {
+
+	public float speed = 0.1f;
+
+	private Vector2 offset = Vector2.zero;
+	private Material mat;
+
+	private void Start () {
+		mat = GetComponent<Renderer> ().material;
+		offset = mat.GetTextureOffset ("_MainTex");
+	}
+
+	private void Update () {
+		offset.x += speed * Time.deltaTime;
+		mat.SetTextureOffset ("_MainTex", offset);
+	}
+}
---
+++ Assets/Scripts/BackgroundScaler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScaler : MonoBehaviour {
+
+	private void Start () {
+		float height = Camera.main.orthographicSize * 2f;
+		float width = height * Screen.width / Screen.height;
+
+		if (gameObject.tag == "background") {
+			transform.localScale = new Vector3 (width, height, 0);
+		}
+
+		if (gameObject.name == "ground") {
+			transform.localScale = new Vector3 (width + 3, 4, 0);
+		}
+	}
+}
